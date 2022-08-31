0b7ac6ec7917459b489fa4bdbea4d72831c3b7c4
GoodDonkey <president20500@gmail.com>
2022-08-31 14:33:13 +0900

    initial commit

---
+++ src/main/java/com/recordy/recordybackend/RecordyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.recordy.recordybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordyBackendApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(RecordyBackendApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/com/recordy/recordybackend/RecordyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recordy.recordybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordyBackendApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
