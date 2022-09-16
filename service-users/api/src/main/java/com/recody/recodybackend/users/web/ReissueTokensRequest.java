d6be0e4b6bd483357aa3d6bfc2470cb3c6d94619
GoodDonkey <president20500@gmail.com>
2022-09-16 19:45:32 +0900

    [RT-10] fix: erased custom annotation

--- service-users/api/src/main/java/com/recody/recodybackend/users/web/ReissueTokensRequest.java
+++ service-users/api/src/main/java/com/recody/recodybackend/users/web/ReissueTokensRequest.java
@@ -1,10 +1,8 @@
 package com.recody.recodybackend.users.web;
 
-import com.recody.recodybackend.common.web.UserBoundDataClass;
 import lombok.Data;
 
 @Data
-@UserBoundDataClass
 public class ReissueTokensRequest {
     
     private String refreshToken;
