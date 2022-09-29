0395cc621056e38d92b07f90d1d9ca4ed23b8eed
GoodDonkey <president20500@gmail.com>
2022-09-29 17:48:12 +0900

    RCDY-86 feat: content search, detail feature
    작품 정보를 가져와서 개인화한다. 지금은 영화정보만 해당.

---
+++ service-catalog/api/src/main/java/com/recody/recodybackend/catalog/features/search/SearchContentResponse.java
@@ -0,0 +1,19 @@
+package com.recody.recodybackend.catalog.features.search;
+
+import com.recody.recodybackend.catalog.PersonalizedContent;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class SearchContentResponse {
+    
+    public List<? extends PersonalizedContent> contents;
+    
+    public SearchContentResponse(List<? extends PersonalizedContent> contents) {
+        this.contents = contents;
+    }
+}
