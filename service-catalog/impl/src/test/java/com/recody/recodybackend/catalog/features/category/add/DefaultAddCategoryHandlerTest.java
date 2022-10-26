7e439777f6c6a01b36f4bc5145b84cb3a277b7f3
GoodDonkey <president20500@gmail.com>
2022-10-26 23:27:37 +0900

    RCDY-273 feat: get categories

---
+++ service-catalog/impl/src/test/java/com/recody/recodybackend/catalog/features/category/add/DefaultAddCategoryHandlerTest.java
@@ -0,0 +1,83 @@
+package com.recody.recodybackend.catalog.features.category.add;
+
+import com.recody.recodybackend.catalog.RecodyCatalogApplication;
+import com.recody.recodybackend.catalog.exceptions.CustomCategoryException;
+import com.recody.recodybackend.catalog.features.CustomCategory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@ExtendWith( MockitoExtension.class )
+@SpringBootTest
+@ActiveProfiles( "test" )
+@ContextConfiguration( classes = RecodyCatalogApplication.class )
+class DefaultAddCategoryHandlerTest {
+    
+    public static final String ICON_URL = "ICON_URL";
+    public static final long USER_ID = 123L;
+    @Autowired
+    AddCategoryHandler addCategoryHandler;
+    
+    @Test
+    @DisplayName( "한 유저당 카테고리는 10개까지 가능하다, 기본 카테고리 5개, 커스텀 카테고리 5개" )
+    void categoryUserCheck() {
+        // given
+        // 4개를 넣는다.
+        for (int i = 0; i < 4; i++) {
+            AddCategory command = newCommandWithSameUser(UUID.randomUUID().toString().substring(0, 7));
+            addCategoryHandler.handle(command);
+        }
+        
+        // when
+        
+        // then
+        
+        assertThatThrownBy(() -> addCategoryHandler.handle(newCommandWithSameUser(UUID.randomUUID().toString())))
+                .isInstanceOf(CustomCategoryException.class);
+    
+    }
+    
+    private static AddCategory newCommandWithSameUser(String name) {
+        return AddCategory.builder().name(name).userId(USER_ID).iconUrl(ICON_URL).build();
+    }
+    
+    @Test
+    @DisplayName( "한 유저는 같은 이름의 카테고리를 추가할 수 없다." )
+    void userCannotAddSameCategory() {
+        // given
+        AddCategory command1 = newCommandWithSameUser("1");
+        AddCategory command2 = newCommandWithSameUser("1");
+        // when
+        CustomCategory handled = addCategoryHandler.handle(command1);
+        
+        // then
+        assertThatThrownBy(() -> addCategoryHandler.handle(command2))
+                .isInstanceOf(CustomCategoryException.class);
+    }
+    
+    @Test
+    @DisplayName( "이름은 null 이거나 빈 값일 수 없다." )
+    void nameCannotBeBlank() {
+        // given
+        AddCategory nullCommand = newCommandWithSameUser(null);
+        AddCategory blankCommand = newCommandWithSameUser("");
+        
+        // when
+        
+        // then
+        assertThatThrownBy(() -> addCategoryHandler.handle(nullCommand))
+                .isInstanceOf(CustomCategoryException.class);
+        assertThatThrownBy(() -> addCategoryHandler.handle(blankCommand))
+                .isInstanceOf(CustomCategoryException.class);
+    }
+    
+}
