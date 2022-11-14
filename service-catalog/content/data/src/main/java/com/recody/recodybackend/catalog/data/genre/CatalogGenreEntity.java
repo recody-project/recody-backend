bff62d03df98ba5c261623ca16ad0f7aae6eafd3
GoodDonkey <president20500@gmail.com>
2022-11-14 15:16:37 +0900

    RCDY-307 fix: genre entity fix

--- service-catalog/content/data/src/main/java/com/recody/recodybackend/catalog/data/genre/CatalogGenreEntity.java
+++ service-catalog/content/data/src/main/java/com/recody/recodybackend/catalog/data/genre/CatalogGenreEntity.java
@@ -2,7 +2,11 @@ package com.recody.recodybackend.catalog.data.genre;
 
 
 import com.recody.recodybackend.catalog.data.category.CategoryEntity;
+import com.recody.recodybackend.catalog.data.user.CatalogUserEntity;
+import com.recody.recodybackend.commonbootutils.data.CustomSequenceIdGenerator;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 import javax.persistence.*;
 
@@ -11,17 +15,32 @@ import javax.persistence.*;
 @NoArgsConstructor( access = AccessLevel.PROTECTED )
 @Builder
 @Getter
-@Table(name = "catalog_genre")
+@Table( name = "catalog_genre" )
 public class CatalogGenreEntity {
     
     @Id
+    @GeneratedValue( generator = "genre_seq", strategy = GenerationType.SEQUENCE )
+    @GenericGenerator( name = "genre_seq",
+                       strategy = "com.recody.recodybackend.commonbootutils.data.CustomSequenceIdGenerator",
+                       parameters = {
+                               @Parameter( name = CustomSequenceIdGenerator.INCREMENT_PARAM, value = "50" ),
+                               @Parameter( name = CustomSequenceIdGenerator.INITIAL_PARAM, value = "100" ),
+                               @Parameter( name = CustomSequenceIdGenerator.VALUE_PREFIX_PARAMETER, value = "g-" ),
+                               @Parameter( name = CustomSequenceIdGenerator.NUMBER_FORMAT_PARAMETER, value = "%d" )} )
     private String id;
     
+    @Column( name = "name", nullable = false )
     private String name;
+    
+    @Column( name = "icon_url" )
+    private String iconUrl;
     
     @ManyToOne
     @JoinColumn( name = "category_id" )
     private CategoryEntity category;
     
+    @ManyToOne
+    @JoinColumn( name = "user_id" )
+    private CatalogUserEntity user;
     
 }
