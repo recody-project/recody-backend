d5a48864b31075773a769ed1fd6d1f71d6d72fed
GoodDonkey <president20500@gmail.com>
2022-11-29 12:24:26 +0900

    RCDY-318 chore: moved user data module

--- service-users/data/src/main/java/com/recody/recodybackend/users/UsersDataConfig.java
+++ service-users/data/src/main/java/com/recody/recodybackend/users/UsersDataConfig.java
@@ -1,4 +1,4 @@
-package com.recody.recodybackend.users.config;
+package com.recody.recodybackend.users;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
