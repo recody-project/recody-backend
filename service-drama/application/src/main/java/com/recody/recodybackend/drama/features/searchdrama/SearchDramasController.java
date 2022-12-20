01c8b8f188f912f6989d4f2b69ebae465e9575bb
GoodDonkey <president20500@gmail.com>
2022-12-20 15:25:13 +0900

    RCDY-325 feat: http response with success body

--- service-drama/application/src/main/java/com/recody/recodybackend/drama/features/searchdrama/SearchDramasController.java
+++ service-drama/application/src/main/java/com/recody/recodybackend/drama/features/searchdrama/SearchDramasController.java
@@ -1,7 +1,9 @@
 package com.recody.recodybackend.drama.features.searchdrama;
 
+import com.recody.recodybackend.common.web.SuccessResponseBody;
 import com.recody.recodybackend.drama.Dramas;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +16,24 @@ public class SearchDramasController {
     
     private final SearchDramaHandler<Dramas> searchDramaHandler;
     
+    private final MessageSource ms;
+    
     @GetMapping( "/api/v1/drama/search" )
-    public Dramas searchDramas(@RequestParam String keyword,
-                               HttpServletRequest request) {
+    public SuccessResponseBody searchDramas(@RequestParam String keyword,
+                                            HttpServletRequest request) {
+        return SuccessResponseBody.builder()
+                                  .message( ms.getMessage( "drama.search.succeeded", null, null, request.getLocale() ) )
+                                  .data( searchDramaHandler.handle(
+                                          SearchDramas.builder()
+                                                      .keyword( keyword )
+                                                      .locale( request.getLocale() )
+                                                      .build() ) )
+                                  .build();
+    }
+    
+    @GetMapping( "/api/v1/drama/search-data" )
+    public Dramas searchDramasData(@RequestParam String keyword,
+                                   HttpServletRequest request) {
         return searchDramaHandler.handle(
                 SearchDramas.builder()
                             .keyword( keyword )
