5c65d3a8bc87eeb880f6bcfeaa6acf37303ecf2f
GoodDonkey <president20500@gmail.com>
2023-01-07 16:40:51 +0900

    RCDY-345 feat: getting person name with default value
    - 영어 이름을 가져오는 로직 추가.
    - 기본값을 받아 줄 수 있도록 메서드 추가.

--- service-drama/tmdb/src/main/java/com/recody/recodybackend/drama/tmdb/TMDBPersonName.java
+++ service-drama/tmdb/src/main/java/com/recody/recodybackend/drama/tmdb/TMDBPersonName.java
@@ -2,6 +2,7 @@ package com.recody.recodybackend.drama.tmdb;
 
 import com.recody.recodybackend.common.exceptions.ApplicationExceptions;
 import lombok.Getter;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.Objects;
@@ -21,19 +22,49 @@ public class TMDBPersonName {
         return new TMDBPersonName( value );
     }
     
-    public static TMDBPersonName firstKoreanNameOf(List<String> names){
+    public static TMDBPersonName firstKoreanNameOf(List<String> names) {
         String foundName = "";
-        if ( Objects.isNull( names ) ){
+        if ( Objects.isNull( names ) ) {
             return new TMDBPersonName( foundName );
         }
         for (String name : names) {
             // 한글, 공백으로 구성된 문자열을 찾는다.
-            if ( Pattern.matches( "^[가-힣\\s]+$", name )){
+            if ( Pattern.matches( "^[가-힣\\s]+$", name ) ) {
                 foundName = name;
                 break;
             }
         }
         return new TMDBPersonName( foundName );
+    }
+    
+    public static TMDBPersonName firstKoreanNameOf(List<String> names, String defaultName) {
+        TMDBPersonName tmdbPersonName = firstKoreanNameOf( names );
+        return hasText( tmdbPersonName ) ? tmdbPersonName : TMDBPersonName.of( defaultName );
+    }
+    
+    public static TMDBPersonName firstEnglishNameOf(List<String> names) {
+        String foundName = "";
+        if ( Objects.isNull( names ) ) {
+            return new TMDBPersonName( foundName );
+        }
+        for (String name : names) {
+            // 한글, 공백으로 구성된 문자열을 찾는다.
+            if ( Pattern.matches( "^[a-zA-Z\\s]+$", name ) ) {
+                foundName = name;
+                break;
+            }
+        }
+        return new TMDBPersonName( foundName );
+    }
+    
+    public static TMDBPersonName firstEnglishNameOf(List<String> names, String defaultName) {
+        TMDBPersonName tmdbPersonName = TMDBPersonName.firstEnglishNameOf( names );
+        return hasText( tmdbPersonName ) ? tmdbPersonName : TMDBPersonName.of( defaultName );
+    }
+    
+    
+    private static boolean hasText(TMDBPersonName tmdbPersonName) {
+        return StringUtils.hasText( tmdbPersonName.getValue() );
     }
     
     @Override
