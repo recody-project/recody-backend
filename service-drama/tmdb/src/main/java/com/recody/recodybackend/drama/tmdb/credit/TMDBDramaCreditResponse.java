832cbe6594eb673d7782e716996700282de8bbef
GoodDonkey <president20500@gmail.com>
2023-01-07 19:04:24 +0900

    RCDY-345 feat: limiting tmdb response and filtering

--- service-drama/tmdb/src/main/java/com/recody/recodybackend/drama/tmdb/credit/TMDBDramaCreditResponse.java
+++ service-drama/tmdb/src/main/java/com/recody/recodybackend/drama/tmdb/credit/TMDBDramaCreditResponse.java
@@ -1,18 +1,39 @@
 package com.recody.recodybackend.drama.tmdb.credit;
 
+import com.recody.recodybackend.drama.tmdb.TMDB;
 import lombok.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-@Getter
 @Setter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor( access = AccessLevel.PROTECTED )
 public class TMDBDramaCreditResponse {
-
+    
     private List<TMDBDramaCast> cast;
     private List<TMDBDramaCrew> crew;
+    
+    
+    /**
+     * @return 부서가 ACTING 이고 최대 5 개까지만 반환.
+     */
+    public List<TMDBDramaCast> getCast() {
+        return cast.stream()
+                   .filter( it -> it.getKnownForDepartment().equals( TMDB.ACTING ) )
+                   .limit( TMDB.ACTOR_MAX_SIZE ).collect( Collectors.toList() );
+    }
+    
+    /**
+     * @return 부서가 DIRECTING 이고 최대 5개까지만 반환.
+     */
+    public List<TMDBDramaCrew> getCrew() {
+        return crew.stream()
+                       .filter( it -> it.getKnownForDepartment().equals( TMDB.DIRECTING ) )
+                       .limit( TMDB.DIRECTOR_MAX_SIZE )
+                       .collect( Collectors.toList() );
+    }
     
     @Override
     public String toString() {
